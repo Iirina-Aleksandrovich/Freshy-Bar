54e7d972b10a708582a31c39e7ac302053a4f42f
Iirina-Aleksandrovich <aleksandrovi4irina@gmail.com>
2023-07-28 16:15:04 +0300

    add javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 const API_URL = "https://verbose-zigzag-noodle.glitch.me/";
-console.log(API_URL);
-const goodsListElem = document.querySelector(".goods__list");
-console.log(goodsListElem);
+
+const getData = () => {
+    const response = fetch(API_URL + "/api/goods")
+}
+
+const init = () => {
+    const goodsListElem = document.querySelector(".goods__list");
+    /* '=' знак присвоить, querySelector ищит по селектору, '.' означает класс */
+    getData();
+}
+
+init();
